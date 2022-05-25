f4549af12e6694d1c873d91485645b8c1a2e9616
Federico <fede.omb.20@virgilio.it>
2022-05-25 10:25:16 +0200

    test layout main activity 3

---
+++ app/src/main/java/fo/pigdm/colors2048/view/GameActivity.java
@@ -0,0 +1,16 @@
+package fo.pigdm.colors2048.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import fo.pigdm.colors2048.R;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
--- app/src/main/java/fo/pigdm/colors2048/view/MainActivity.java
+++ app/src/main/java/fo/pigdm/colors2048/view/MainActivity.java
@@ -1,8 +1,12 @@
-package fo.pigdm.colors2048;
+package fo.pigdm.colors2048.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import fo.pigdm.colors2048.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startGameActivity(View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+
     }
 }
