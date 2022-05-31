85a18deea3456f56d1a5cc232e968b025adcc510
Federico <fede.omb.20@virgilio.it>
2022-05-31 23:02:12 +0200

    aggiunta immersive mode su mainactivity e gameactivity

--- app/src/main/java/fo/pigdm/colors2048/view/GameActivity.java
+++ app/src/main/java/fo/pigdm/colors2048/view/GameActivity.java
@@ -1,6 +1,9 @@
 package fo.pigdm.colors2048.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 
 import android.os.Bundle;
 
@@ -14,5 +17,19 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        hideSystemBars();
+    }
+
+
+    private void hideSystemBars() {
+        WindowInsetsControllerCompat windowInsetsController = ViewCompat.getWindowInsetsController(getWindow().getDecorView());
+        if (windowInsetsController == null) {
+            return;
+        }
+        // Configure the behavior of the hidden system bars
+        windowInsetsController.setSystemBarsBehavior(WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        );
+        // Hide both the status bar and the navigation bar
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars());
     }
 }
--- app/src/main/java/fo/pigdm/colors2048/view/MainActivity.java
+++ app/src/main/java/fo/pigdm/colors2048/view/MainActivity.java
@@ -1,6 +1,9 @@
 package fo.pigdm.colors2048.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,11 +17,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        hideSystemBars();
     }
 
     public void startGameActivity(View view) {
         Intent intent = new Intent(this, GameActivity.class);
         startActivity(intent);
 
+    }
+
+    private void hideSystemBars() {
+        WindowInsetsControllerCompat windowInsetsController = ViewCompat.getWindowInsetsController(getWindow().getDecorView());
+        if (windowInsetsController == null) {
+            return;
+        }
+        // Configure the behavior of the hidden system bars
+        windowInsetsController.setSystemBarsBehavior(WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        );
+        // Hide both the status bar and the navigation bar
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars());
     }
 }
